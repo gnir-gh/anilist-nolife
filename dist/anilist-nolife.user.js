d100f8f012a6e04ac09392c89bb2d5481a138884
gnir-gh <gnir.ring.gnir.ring@proton.me>
2023-06-17 03:52:00 +0100

    [release] 0.1.2

--- dist/anilist-nolife.user.js
+++ dist/anilist-nolife.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name anilist-nolife
 // @description Quality of no life improvement on AniList
-// @version 0.1.1
+// @version 0.1.2
 // @author gnir
 // @homepage https://github.com/gnir-gh
 // @match https://anilist.co/*
